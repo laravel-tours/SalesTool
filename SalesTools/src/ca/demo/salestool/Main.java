e93bc0981fe081f3601b4a13428b0e961721cc23
shiam <shiamplus@gmail.com>
2016-05-17 03:54:26 +0600

    Initial checkin for sales tools

---
+++ SalesTools/src/ca/demo/salestool/Main.java
@@ -0,0 +1,14 @@
+package ca.demo.salestool;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		DisplayGreeting();
+		data.display();
+	}
+
+	private static void DisplayGreeting() {
+		System.out.println("Hello Happy sales people.");
+		System.out.println("This apps shows apps data.");
+	}
+}
